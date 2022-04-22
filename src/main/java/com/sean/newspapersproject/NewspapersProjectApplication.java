718fb6b7896b64dea59c93a519487c1fc77bf488
Gleb <asglus7@gmail.com>
2022-04-22 12:50:15 +0300

    Initial commit of the project

---
+++ src/main/java/com/sean/newspapersproject/NewspapersProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sean.newspapersproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewspapersProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewspapersProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sean/newspapersproject/NewspapersProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sean.newspapersproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewspapersProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
